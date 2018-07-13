c251d9f1882c6f2103140c62f1a161d1cd112362
Daniel Schulz <faulolio@gmail.com>
2018-07-13 16:08:16 +0200

    ServerEnvironment::cleanCacheDirectory

--- src/Slothsoft/Core/IO/RecursiveFileIterator.php
+++ src/Slothsoft/Core/IO/RecursiveFileIterator.php
@@ -2,19 +2,46 @@
 declare(strict_types = 1);
 namespace Slothsoft\Core\IO;
 
+use InvalidArgumentException;
 use RecursiveDirectoryIterator;
 use RecursiveFilterIterator;
 use RecursiveIteratorIterator;
 
-class RecursiveFileIterator extends RecursiveFilterIterator
+class RecursiveFileIterator 
 {
-    public static function iterateDirectory(string $directory) : iterable {
-        $directoryIterator = new RecursiveDirectoryIterator($directory);
-        $filteredIterator = new self($directoryIterator);
-        return new RecursiveIteratorIterator($filteredIterator);
+    public static function iterateDirectoriesAndFiles(string $directory) : iterable {
+        if (!is_dir($directory)) {
+            throw new InvalidArgumentException("Directory '$directory' does not exist.");
+        }
+        $directory = realpath($directory);
+        $iterator = new RecursiveDirectoryIterator($directory);
+        $iterator = new class($iterator) extends RecursiveFilterIterator {
+            public function accept() : bool {
+                $name = $this->current()->getFilename();
+                return $name !== '..';
+            }
+        };
+        $iterator = new RecursiveIteratorIterator($iterator);
+        foreach ($iterator as $file) {
+            $path = $file->getRealPath();
+            if ($path !== $directory) {
+                yield $path;
+            }
+        }
     }
-    public function accept() : bool {
-        return $this->current()->getFilename()[0] !== '.';
+    public static function iterateFiles(string $directory) : iterable {
+        foreach (self::iterateDirectoriesAndFiles($directory) as $file) {
+            if (is_file($file)) {
+                yield $file;
+            }
+        }
+    }
+    public static function iterateDirectories(string $directory) : iterable {
+        foreach (self::iterateDirectoriesAndFiles($directory) as $file) {
+            if (is_dir($file)) {
+                yield $file;
+            }
+        }
     }
 }
 

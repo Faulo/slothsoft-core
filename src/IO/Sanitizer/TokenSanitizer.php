f79dbb547da9de6c7c0cc560ff10e98420d002c9
Daniel Schulz <faulolio@gmail.com>
2020-12-27 19:38:06 +0100

    Introduce TokenSanitizer

---
+++ src/IO/Sanitizer/TokenSanitizer.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Core\IO\Sanitizer;
+
+class TokenSanitizer implements SanitizerInterface {
+
+    private $default;
+
+    public function __construct(array $default = []) {
+        $this->default = $default;
+    }
+
+    public function apply($value) {
+        if (is_string($value)) {
+            $result = [];
+            foreach (preg_split('~\s+~', $value) as $val) {
+                $val = trim($val);
+                if (strlen($val)) {
+                    $result[] = $val;
+                }
+            }
+            return $result;
+        }
+        if (is_array($value)) {
+            return $value;
+        }
+        return $this->getDefault();
+    }
+
+    public function getDefault() {
+        return $this->default;
+    }
+}
+
---
+++ tests/IO/Sanitizer/TokenSanitizerTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Core\IO\Sanitizer;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * TokenSanitizerTest
+ *
+ * @see TokenSanitizer
+ *
+ * @todo auto-generated
+ */
+class TokenSanitizerTest extends TestCase {
+
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(TokenSanitizer::class), "Failed to load class 'Slothsoft\Core\IO\Sanitizer\TokenSanitizer'!");
+    }
+}

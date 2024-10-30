b9f79c21eb4dbaa25c8f8d1ebaa02933f7a7a93f
Daniel Schulz <faulolio@gmail.com>
2024-10-30 16:39:23 +0100

    test: LeanElement

--- tests/XML/LeanElementTest.php
+++ tests/XML/LeanElementTest.php
@@ -16,4 +16,63 @@ class LeanElementTest extends TestCase {
     public function testClassExists(): void {
         $this->assertTrue(class_exists(LeanElement::class), "Failed to load class 'Slothsoft\Core\XML\LeanElement'!");
     }
+
+    public function testThatTagGetsSerialized(): void {
+        $tag = 'test';
+
+        $sut = LeanElement::createOneFromArray($tag, [], []);
+
+        $actual = unserialize(serialize($sut));
+
+        $this->assertEquals($tag, $actual->getTag());
+    }
+
+    public function testThatAttributeGetsSerialized(): void {
+        $key = 'hello';
+        $value = 'world';
+
+        $sut = LeanElement::createOneFromArray('tag', [
+            $key => $value
+        ], []);
+
+        $actual = unserialize(serialize($sut));
+
+        $this->assertEquals($value, $actual->getAttribute($key));
+    }
+
+    public function testThatChildGetsSerialized(): void {
+        $tag = 'child';
+        $key = 'hello';
+        $value = 'world';
+
+        $child = LeanElement::createOneFromArray($tag, [
+            $key => $value
+        ], []);
+
+        $sut = LeanElement::createOneFromArray('tag', [], [
+            $child
+        ]);
+
+        $actual = unserialize(serialize($sut));
+
+        $this->assertEquals($value, $actual->getChildByTag($tag)
+            ->getAttribute($key));
+    }
+
+    public function testGetChildByTag(): void {
+        $tag = 'child';
+        $key = 'hello';
+        $value = 'world';
+
+        $child = LeanElement::createOneFromArray($tag, [
+            $key => $value
+        ], []);
+
+        $sut = LeanElement::createOneFromArray('tag', [], [
+            $child
+        ]);
+
+        $this->assertEquals($value, $sut->getChildByTag($tag)
+            ->getAttribute($key));
+    }
 }

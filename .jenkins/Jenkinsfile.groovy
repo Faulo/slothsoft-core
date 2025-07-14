b72f8f526d6631dc1a6b7405c76845a2febab5ae
Daniel Schulz <faulolio@gmail.com>
2025-07-14 18:20:35 +0200

    devops: windows tests

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -1,13 +1,52 @@
 pipeline {
-	agent {
-		label 'docker'
-	}
+	agent none
 	options {
 		disableConcurrentBuilds()
 		disableResume()
 	}
 	stages {
-		stage('Init') {
+		stage('Linux') {
+			agent {
+				label 'docker && linux'
+			}
+			steps {
+				script {
+					def versions = [
+						"7.4",
+						"8.0",
+						"8.1",
+						"8.2",
+						"8.3"
+					];
+
+					for (version in versions) {
+						def image = "faulo/farah:${version}"
+
+						stage("PHP: ${version}") {
+							callShell "docker pull ${image}"
+
+							docker.image(image).inside {
+								stage('Install dependencies') {
+									callShell 'composer install'
+								}
+								stage('Run Tests') {
+									catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE') {
+										callShell 'composer exec phpunit -- --log-junit report.xml'
+									}
+									if (fileExists('report.xml')) {
+										junit 'report.xml'
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		stage('Windows') {
+			agent {
+				label 'docker && windows'
+			}
 			steps {
 				script {
 					def versions = [

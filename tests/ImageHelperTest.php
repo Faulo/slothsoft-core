6d3b0496ce6d494b19dd72c3cad71e196168eedc
Daniel Schulz <faulolio@gmail.com>
2025-09-02 22:10:50 +0200

    test: ImageHelper

--- tests/ImageHelperTest.php
+++ tests/ImageHelperTest.php
@@ -3,17 +3,39 @@ declare(strict_types = 1);
 namespace Slothsoft\Core;
 
 use PHPUnit\Framework\TestCase;
+use Slothsoft\Core\IO\FileInfoFactory;
 
 /**
  * ImageHelperTest
  *
  * @see ImageHelper
- *
- * @todo auto-generated
  */
 class ImageHelperTest extends TestCase {
 
     public function testClassExists(): void {
         $this->assertTrue(class_exists(ImageHelper::class), "Failed to load class 'Slothsoft\Core\ImageHelper'!");
+    }
+
+    /**
+     *
+     * @dataProvider imageProvider
+     * @test
+     */
+    public function testConvertToPng(string $inFile, string $expectedFile, ?int $alphaColorIndex) {
+        $actualFile = FileInfoFactory::createTempFile();
+
+        ImageHelper::convertToPng(FileInfoFactory::createFromPath($inFile), $actualFile, $alphaColorIndex);
+
+        $this->assertFileEquals($expectedFile, (string) $actualFile);
+    }
+
+    public static function imageProvider(): array {
+        return [
+            'TGA to PNG' => [
+                'test-files/ImageHelper/6.tga',
+                'test-files/ImageHelper/6.png',
+                0
+            ]
+        ];
     }
 }

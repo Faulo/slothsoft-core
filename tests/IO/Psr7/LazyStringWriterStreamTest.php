76100cf9ce6d03e37a0be736d16e79e6074d3acd
Daniel Schulz (Estelle) <faulolio@gmail.com>
2026-01-06 15:51:02 +0100

    test: LazyStringWriterStream

---
+++ tests/IO/Psr7/LazyStringWriterStreamTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Core\IO\Psr7;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * LazyStringWriterStreamTest
+ *
+ * @see LazyStringWriterStream
+ *
+ * @todo auto-generated
+ */
+final class LazyStringWriterStreamTest extends TestCase {
+    
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(LazyStringWriterStream::class), "Failed to load class 'Slothsoft\Core\IO\Psr7\LazyStringWriterStream'!");
+    }
+}

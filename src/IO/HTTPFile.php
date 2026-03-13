13d3b54e5af5ca710ada69bf9e7669b66e1c2e06
Daniel Schulz <faulolio@gmail.com>
2026-03-13 14:12:34 +0100

    fix: backtick

--- src/IO/HTTPFile.php
+++ src/IO/HTTPFile.php
@@ -171,7 +171,7 @@ class HTTPFile implements FileWriterInterface {
             $refererURI = sprintf('Referer: %s://%s%s', $param['scheme'], $param['host'], $param['path']);
             $filePath = self::getTempFile();
             $downloadExec = sprintf(self::CURL_COMMAND, escapeshellarg(urldecode($url)), escapeshellarg($filePath), escapeshellarg($refererURI));
-            `$downloadExec`;
+            shell_exec($downloadExec);
             $ret = file_exists($filePath) ? self::createFromPath($filePath, $fileName) : null;
         } else {
             @$data = file_get_contents($url);

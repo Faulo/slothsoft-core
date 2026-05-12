d117eaf9ca6d03ffea6ca8f5ae54b9f01917739a
Daniel Schulz <faulolio@gmail.com>
2026-05-12 12:47:50 +0200

    test: open

--- tests/XMLHttpRequestTest.php
+++ tests/XMLHttpRequestTest.php
@@ -1,13 +1,37 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
+
 namespace Slothsoft\Core;
 
+use PHPUnit\Framework\Constraint\IsEqual;
 use PHPUnit\Framework\TestCase;
 
-class XMLHttpRequestTest extends TestCase {
-    
-    public function testContructor() {
-        $request = new XMLHttpRequest();
-        $this->assertEquals(XMLHttpRequest::UNSENT, $request->readyState);
+final class XMLHttpRequestTest extends TestCase {
+
+    /**
+     * @test
+     */
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(XMLHttpRequest::class), "Failed to load class 'Slothsoft\Core\XMLHttpRequest'!");
+    }
+
+    /**
+     * @test
+     */
+
+    public function testConstructor() {
+        $sut = new XMLHttpRequest();
+
+        $this->assertThat($sut->readyState, new IsEqual(XMLHttpRequest::UNSENT));
+    }
+
+    /**
+     * @test
+     */
+    public function testDownload() {
+        $sut = new XMLHttpRequest();
+        $sut->open('GET', 'https://www.w3.org', false);
+
+        $this->assertThat($sut->readyState, new IsEqual(XMLHttpRequest::OPENED));
     }
 }
